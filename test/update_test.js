369aac16ab0c2435ee1446c06874c1ba661ca48b
Bugga86 <parab.rohit@gmail.com>
2017-07-06 11:31:05 +0530

    new Updates test

--- test/update_test.js
+++ test/update_test.js
@@ -4,7 +4,7 @@ const User = require('../src/user');
 describe('Updating a value', () => {
 
 beforeEach((done) => {
-  joe = new User({name: 'Joe'});
+  joe = new User({name: 'Joe',postCount: '1'});
   joe.save()
   .then(() => done());
 });
@@ -38,5 +38,23 @@ function assertName(operation, done){
 
    it('A model class can find and update',(done) => {
      assertName(User.findByIdAndUpdate(joe._id,{name: 'Rohit'}),done);
-   })
+   });
+
+   it('A user can be incremented by one', (done) => {
+     User.update({name: 'Joe'},{$mul: {postCount: 1}})
+     .then(() => User.findOne({name: 'Joe'}))
+     .then((user) => {
+       assert(user.postCount === 1 );
+       done();
+     });
+   });
+
+   it('user can be increemented',(done) => {
+     User.update({name: 'Joe'},{$inc: {postCount: -1}})
+     .then(() => User.findOne({name: 'Joe'})
+     .then((user) => {
+       assert(user.postCount === 0);
+       done();
+   }));
+ });
  });
