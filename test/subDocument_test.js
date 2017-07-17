ebc54f963a7338975880cdfa89e6291e79722678
Bugga86 <parab.rohit@gmail.com>
2017-07-17 12:53:46 +0530

    Embedded Documents

--- test/subDocument_test.js
+++ test/subDocument_test.js
@@ -16,5 +16,23 @@ describe('Subdocument',() => {
       done();
     });
 
+    it('can add multiple Subdocuments', (done) => {
+      const joe = new User({
+          name : 'Joe',
+          posts: []
+      });
+      joe.save()
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        user.posts.push({title: 'Team leader'});
+        return user.save();
+      })
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        assert(user.posts[0].title === 'Team leader');
+        done();
+      });
+    });
+
   });
 });
